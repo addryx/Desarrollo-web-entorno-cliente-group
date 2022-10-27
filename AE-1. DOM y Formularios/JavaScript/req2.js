c192f54b9ee34bfaebf3bb93246970abde853b5a
Adrián Fernández <adrianfernandezalv@gmail.com>
2022-10-27 20:31:16 +0200

    Comentarios de las funciones añadidos

--- AE-1. DOM y Formularios/JavaScript/req2.js
+++ AE-1. DOM y Formularios/JavaScript/req2.js
@@ -1,6 +1,11 @@
 function precioTotal() {
-      
+    
+    // Creamos la variable precio para darle un valor al finalizar todas las sumas.
+
     var precio = 0
+
+    // En este if le estamos preguntando si el tamaño en cuestión está checkeado, si lo está le añade el importe correspondiente al precio,
+    // si no lo está pasa al siguiente y vuelve a hacer la misma comprobación.
 
     if(tam_g.checked){
         precio = 15
@@ -10,7 +15,12 @@ function precioTotal() {
         precio = 5
     }
     
+    // Creamos una variable a la cual le añadimos el valor correspondiente a los input con name = "ingredientes"
+
     var ingred = document.getElementsByName("ingredientes")
+
+    // Creamos un for para que recorra todos los input con name = "ingredientes" (a través de la variable creada), si el ingrediente [i] está checkeado, 
+    // le añade +1 al precio, hace una y otra vez este proceso hasta que el for ha recorrido todos los inputs.
 
     for(var i=0; i<ingred.length; i++){
         if(ingred[i].checked){
@@ -18,10 +28,13 @@ function precioTotal() {
         }
     }
 
+    // Sacamos el precio total a través de una alerta al usuario.
+
     alert("Precio: "+precio)
-    console.log("Precio: "+precio)
 }
 
-window.onload = function(){
-    formulario.onsubmit = precioTotal
-}
+    // Con el windows onload le estamos comunicando que cuando el formulario sea enviado (submit a través del botón) ejecute la función precioTotal().
+
+    window.onload = function(){
+        formulario.onsubmit = precioTotal
+    }
