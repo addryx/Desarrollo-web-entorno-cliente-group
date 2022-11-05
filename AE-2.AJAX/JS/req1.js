7077c538b76a6c333b1bb5429620cba5fc92dddc
Lierni <liernibel@hotmail.es>
2022-11-05 19:06:23 +0100

    creada la tabla

--- AE-2.AJAX/JS/req1.js
+++ AE-2.AJAX/JS/req1.js
@@ -13,7 +13,9 @@ const RECURSO = "pizzas.json"
         xmlHttp.onreadystatechange = function () {
 
             // Hay muchas maneras desarrollar la función pero lo hacemos así para controlar mejor. 
-            // Solo procesaremos la respuesta cuando ésta esté completa y sea correcta. Entonces: 
+            // Solo procesaremos la respuesta cuando ésta esté completa y sea correcta. 
+            // Hay que tener en cuenta que la respuesta nos llega en formato texto, después habrá que parsearlo a JSON. 
+            // Entonces: 
             // Si el estado es igual a 4 (estado completo), ejecutas lo que viene después.
             // Si además de estar completo, el estado es 200 (OK), entonces obtenemos el valor en texto. 
             if (this.readyState == 4) {
@@ -38,13 +40,41 @@ const RECURSO = "pizzas.json"
         //Podemos hacer lo contrario con "JSON.stringify(obj)"
         console.log(objetoJson)
 
-        //Lo suyo seria crear objetos con el DOM, esto esta regulín
-        //pero como ejemplo sencillo vale
-        var table = "<tr><th>Ingredientes</th><th>Tamaños</th></tr>";
-        var arraypIZZAs = objetoJson.PIZZAS;//Ojo mayusculas y minusculas, es como este en el json original
-        
+        // Creamos una tabla con los títulos "Ingredientes" y "Tamaños". 
+        var table = document.createElement("table");
+        var tr = document.createElement("tr");
+        var thIngredientes = document.createElement("th");
+        var thTamaños = document.createElement("th");
+        table.id = "tabla";
+        tr.id = "tr";
+        thIngredientes.id = "thIngredientes";
+        thTamaños.id = "thTamaños";
+
+        var textIngredientes = document.createTextNode("Ingredientes");
+        var textTamaños = document.createTextNode("Tamaños");
+
+        thIngredientes.appendChild(textIngredientes);
+        thTamaños.appendChild(textTamaños);
+        tr.appendChild(thIngredientes);
+        tr.appendChild(thTamaños);
+        table.appendChild(tr);
+
+        var arrayPIZZAs = objetoJson.PIZZAS;//Ojo mayusculas y minusculas, es como este en el json original
+
+        var tr1 = documento.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        tr1.id = "tr1";
+        td1.id = "td1";
+        td2.id = "td2";
+
+        // th = Título columna
+        // tr = fila
+        // td = celda
+
+
         //Iteramos el array de pizzas y formamos las filas y columnas
-        for(let pizza of arraypIZZAs){
+        for(let pizza of arrayPIZZAs){
             table += "<tr><td>" + pizza.INGREDIENTES + "</td>" + 
                 "<td>" + pizza.TAMAÑOS + "</td></tr>";
         }
