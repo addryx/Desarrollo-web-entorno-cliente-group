90647ea970928f360250894def8be092b4b05ec1
Adrián Fernández <adrianfernandezalv@gmail.com>
2022-12-02 08:27:25 +0100

    update

--- ae3-angular/src/app/login/login.component.ts
+++ ae3-angular/src/app/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
   public verificarUsuario(){
     for(let i = 0; i < this.listaUsuarios.length; i++){
       if(this.username == this.listaUsuarios[i].username && this.password == this.listaUsuarios[i].password){
-        alert('Usuario exíste.')
+        alert('EXISTE')
         break
       } else {
         alert('Usuario no exíste.')
